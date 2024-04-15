7aca996767440476892b0ed9863d5a256266ec18
Candice Cai <caixy.candice2016@outlook.com>
2024-04-15 14:54:31 -0400

    add getUrl

---
+++ engine/getURLs.js
@@ -0,0 +1,60 @@
+#!/usr/bin/env node
+
+const readline = require('readline');
+const {JSDOM} = require('jsdom');
+const {URL} = require('url');
+
+// Example usage:
+
+const url = process.argv[2];
+let baseURL = '';
+if (url.slice(-5)=='.html') {
+  baseURL = url.slice(0, -10);
+} else if (baseURL.slice(-1)!='/') {
+  baseURL = url + '/';
+} else {
+  baseURL = url;
+}
+
+const rl = readline.createInterface({
+  input: process.stdin,
+});
+
+// TODO some code
+// console.log(baseURL)
+
+const extractUrls = (htmlContent, baseUrl) => {
+  const dom = new JSDOM(htmlContent);
+  const document = dom.window.document;
+
+  const urls = [];
+  const anchors = document.querySelectorAll('a');
+
+  anchors.forEach((anchor) => {
+    const href = anchor.getAttribute('href');
+    if (href) {
+      const absoluteUrl = new URL(href, baseUrl).toString();
+      urls.push(absoluteUrl);
+    }
+  });
+
+  return urls;
+};
+
+
+const printedUrls = new Set();
+
+rl.on('line', (line) => {
+  // TODO some code
+  const urls = extractUrls(line, baseURL);
+  urls.forEach((url) => {
+    if (!printedUrls.has(url)) {
+      console.log(url);
+      printedUrls.add(url);
+    }
+  });
+});
+
+rl.on('close', () => {
+  // TODO some code
+});
---
+++ engine/getUrl.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+T_FOLDER=${T_FOLDER:-t}
+R_FOLDER=${R_FOLDER:-}
+
+cd "$(dirname "$0")/..$R_FOLDER" || exit 1
+
+echo "$T_FOLDER"
+echo "$R_FOLDER"
+
+DIFF=${DIFF:-diff}
+
+url="https://cs.brown.edu/courses/csci1380/sandbox/2/"
+
+curl -k $url | engine/getURLs.js $url
+
+# if $DIFF <(cat "$T_FOLDER"/d/d9.txt | c/getURLs.js $url | sort) <(sort "$T_FOLDER"/d/d10.txt) > /dev/null;
+# then
+#     echo "$0 success: URL sets are identical"
+# else
+#     echo "$0 failure: URL sets are not identical"
+# fi
