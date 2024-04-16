86d1e7e4acf61c20e2a6186d35aa87c56773dcf4
Candice Cai <caixy.candice2016@outlook.com>
2024-04-16 19:41:12 +0000

    more scratch work

--- engine/getUrl.sh
+++ engine/getUrl.sh
@@ -4,14 +4,11 @@ R_FOLDER=${R_FOLDER:-}
 
 cd "$(dirname "$0")/..$R_FOLDER" || exit 1
 
-echo "$T_FOLDER"
-echo "$R_FOLDER"
-
-DIFF=${DIFF:-diff}
 
 url="https://cs.brown.edu/courses/csci1380/sandbox/2/"
 
-curl -k $url | engine/getURLs.js $url
+# curl -k $url | engine/getURLs.js $url
+node engine/getURLs.js $url
 
 # if $DIFF <(cat "$T_FOLDER"/d/d9.txt | c/getURLs.js $url | sort) <(sort "$T_FOLDER"/d/d10.txt) > /dev/null;
 # then
