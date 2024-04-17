e9585a6b0bc8fbec6f1a27d32199adba6b1c5a63
root <hangji_li@brown.edu>
2024-04-17 17:10:04 +0000

    figuring how to import natural

--- distribution.js
+++ distribution.js
@@ -2,6 +2,13 @@
 
 const util = require('./distribution/util/util.js');
 const args = require('yargs').argv;
+// const { PorterStemmer } = require('natural');
+// console.log(PorterStemmer);
+// const stemmer = require('lancaster-stemmer')
+// const { PorterStemmer } = require('natural');
+// console.log('successfully imported natural');
+// console.log(natural);
+// console.log(stemmer);
 
 
 // Default configuration
@@ -33,9 +40,9 @@ if (args.config) {
   let nodeConfig = util.deserialize(args.config);
   global.nodeConfig.ip = nodeConfig.ip ? nodeConfig.ip : global.nodeConfig.ip;
   global.nodeConfig.port = nodeConfig.port ?
-        nodeConfig.port : global.nodeConfig.port;
+    nodeConfig.port : global.nodeConfig.port;
   global.nodeConfig.onStart = nodeConfig.onStart ?
-        nodeConfig.onStart : global.nodeConfig.onStart;
+    nodeConfig.onStart : global.nodeConfig.onStart;
 }
 
 const distribution = {
@@ -44,23 +51,27 @@ const distribution = {
   node: require('./distribution/local/node.js'),
 };
 
+// distribution.stemmer = PorterStemmer;
+
 global.distribution = distribution;
+
+// global.natural = natural;
 
 distribution['all'] = {};
 distribution['all'].status =
-    require('./distribution/all/status')({gid: 'all'});
+  require('./distribution/all/status')({ gid: 'all' });
 distribution['all'].comm =
-    require('./distribution/all/comm')({gid: 'all'});
+  require('./distribution/all/comm')({ gid: 'all' });
 distribution['all'].gossip =
-    require('./distribution/all/gossip')({gid: 'all'});
+  require('./distribution/all/gossip')({ gid: 'all' });
 distribution['all'].groups =
-    require('./distribution/all/groups')({gid: 'all'});
+  require('./distribution/all/groups')({ gid: 'all' });
 distribution['all'].routes =
-    require('./distribution/all/routes')({gid: 'all'});
+  require('./distribution/all/routes')({ gid: 'all' });
 distribution['all'].mem =
-    require('./distribution/all/mem')({gid: 'all'});
+  require('./distribution/all/mem')({ gid: 'all' });
 distribution['all'].store =
-    require('./distribution/all/store')({gid: 'all'});
+  require('./distribution/all/store')({ gid: 'all' });
 
 const {JSDOM} = require('jsdom');
 global.JSDOM = JSDOM;
