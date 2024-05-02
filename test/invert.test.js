29dc84be2ed3e1aaac3fa0d93a8d2f437cf5e252
Candice Cai <caixy.candice2016@outlook.com>
2024-05-02 04:12:34 +0000

    modified local comm

--- test/invert.test.js
+++ test/invert.test.js
@@ -29,8 +29,8 @@ let localServer = null;
 */
 
 const nodes = [];
-for (let i = 1; i <= ipAddresses.length; i++) {
-    nodes.push({ ip: ipAddresses[i-1], port: startPort + i });
+for (let i = 1; i <= 5; i++) {
+    nodes.push({ ip: '127.0.0.1', port: startPort + i });
 }
 
 
@@ -192,7 +192,7 @@ test('(25 pts) Inverted index wordflow', (done) => {
         let configuration = { map: m1, reduce: r1, storeGroup: 'invertedIdx' };
 
         let execMr = global.promisify(distribution.downloadText.mr.exec)
-        let batchSize = 3;
+        let batchSize =5;
         let totalNumKeys = urlKeys.length;//urlKeys.length
         for (let i = 0; i < totalNumKeys; i += batchSize) {
             if (i + batchSize > totalNumKeys) {
